079e91fc8331983ee46bf8b76bc725b093a0babe
Lucas Prado <42065138+Salobre@users.noreply.github.com>
2019-05-26 16:41:45 -0300

    Update Tp.h

--- Tp.h
+++ Tp.h
@@ -1,16 +1,25 @@
 #ifndef TP_H
-#define Tp_H
+#define TP_H
 #include <iostream>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <map>
+#include <set>
+
+using namespace std;
+using std::string;
 class File_reader{
 public:
-void Ler(const string)const;
+void  Ler()const;
+
+void Inserir(string s)const;
+
+void Indice_invertido();
 
 private:
 map<string,set<string>> indice;
+int doc_number;
 
 };
 
