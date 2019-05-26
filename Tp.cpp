98d2768848ca3134ba5655e2ae2c771d5e8b353c
Lucas Prado <42065138+Salobre@users.noreply.github.com>
2019-05-26 16:40:30 -0300

    Update Tp.cpp

--- Tp.cpp
+++ Tp.cpp
@@ -1,18 +1,40 @@
 #include "TP.h"
 
 
-File_reader::void Ler(const string)const{
+void File_reader::Ler()const{
 for(int i = 1; 1;i++){
-    ifstream doc
+    ifstream doc;
     doc.open("d" + to_string(i) +".txt");
-    if (!doc.is_open && i==1){
-            cout<<"Falha na leitura"<<endl
+    if (!doc.is_open() && i==1){
+        cout<<"Falha na leitura"<<endl;
         break;
     }
-    if (!doc.is_open){
+     else if (!doc.is_open()){
         break;
 }
-doc.close;
+    string palavra;
+    while(!doc.eof()){
+        doc >> palavra;
+        set<string> valor = {"d" + to_string(i) +".txt"};
+        pair<map<string,set<string>>::iterator,bool> teste;
+        teste = indice.insert(std::pair<string,set<string>>(palavra,valor));
+        if(!teste.second){
+            indice[palavra].insert(valor);
+        }
+
+        }
+    }
+
+
+doc.close();
+};
+
+void File_reader::Indice_invertido()const{
+    map<string,set<string>>::iterator itr;
+    for(auto itr : indice){
+        cout<< itr->first<<"->"<<itr->second<<endl;
+
+    }
 }
 
-};
+}
