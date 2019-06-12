82ab978dbc12f83ab62318b54cf89aceae01d909
Lucas Prado <42065138+Salobre@users.noreply.github.com>
2019-06-11 23:43:21 -0300

    Update Tp.cpp

--- Tp.cpp
+++ Tp.cpp
@@ -1,18 +1,67 @@
-#include "TP.h"
+#include "Indice_Invertido.h"
+
+using namespace std;
+
+void File_reader::Ler() {
+	ifstream doc;
+	for (int i = 1; 1; i++) {
+
+		doc.open("d" + to_string(i) + ".txt");
+		if (!doc.is_open() && i == 1) {
+			cout << "Falha na leitura" << endl;
+			break;
+		}
+		if (!doc.is_open()) {
+			break;
+		}
+		
+		string palavra;
+		while (!doc.eof()) {
+			doc >> palavra;
+			transform(palavra.begin(), palavra.end(), palavra.begin(), ::tolower);
+			
+			set<string> valor = { "d" + to_string(i) + ".txt" };
+			pair<map<string, set<string>>::iterator, bool> teste;
+			teste = indice.insert(pair<string, set<string>>(palavra, valor));
+			
+
+			if (!teste.second) {
+				string novo = "d" + to_string(i) + ".txt";
+			
+				indice[palavra].insert(novo);
+			}
+
+		}
+		doc.close();
+	}
 
 
-File_reader::void Ler(const string)const{
-for(int i = 1; 1;i++){
-    ifstream doc
-    doc.open("d" + to_string(i) +".txt");
-    if (!doc.is_open && i==1){
-            cout<<"Falha na leitura"<<endl
-        break;
-    }
-    if (!doc.is_open){
-        break;
-}
-doc.close;
-}
-
+	
 };
+
+void File_reader::Imprimir() {
+	
+	map<string, set<string>>::iterator itr;
+	for (itr = indice.begin(); itr != indice.end(); itr++) {
+		cout << itr->first << "->";
+		for (auto e = itr->second.begin(); e != itr->second.end(); e++) {
+			
+			cout << *e <<"->"<< endl;
+
+		}
+
+	}
+};
+
+int File_reader::doc_number(string c) {
+	int count = 0;
+	map<string, set<string>> aux;
+	aux = indice;
+	map<string, set<string>>::iterator itr;
+	count = indice[c].size();
+		return count;
+	}
+
+
+
+
