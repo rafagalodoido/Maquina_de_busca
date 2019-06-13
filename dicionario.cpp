4e6db5166bcc646ae47a178bdcb07e8e6884e1db
Lucas Prado <42065138+Salobre@users.noreply.github.com>
2019-06-12 23:04:15 -0300

    Update dicionario.cpp

--- dicionario.cpp
+++ dicionario.cpp
@@ -2,23 +2,43 @@
 #include "dicionario.h"
 using std::string;
 
- double dicionario::idf(File_reader c) {
+int dicionario::idf(string palavra) {
 	//precisa definir o docs
-	 string palavra;
-	 double freq;
-	 double idf;
-	 map<string, set<string>>::iterator itr;
-	 map<string, double> aux;
-	 for (itr = indice.begin(); itr != indice.end(); itr++) {
-		 palavra = itr->first;
-		 N = c.doc_quantity();
-		 nt = c.doc_number(palavra);
-		 freq = N / nt;
-		 idf  = log2(freq);
-		 aux.insert(pair<string, double>(palavra, idf));
-	 }
-	
+	indice_.Ler();
+	string palavra;
+	int freq;
+	int idf;
+	map<string, set<string>>::iterator itr;
+	map<string,int> aux;
+	for (itr = indice_.mapa().begin(); itr != indice_.mapa().end(); itr++) {
+		palavra = itr->first;
+		N = indice_.doc_quantity();
+		nt = indice_.doc_number(palavra);
+		freq = N / nt;
+		idf = log2(freq);
+		aux.insert(pair<string, int>(palavra, idf));
+	}
+
 	if (nt == 0) {
 		idf = 0;
 		cout << "palavra não encontrada." << endl;
 	}
+	return idf;
+}
+int dicionario::tf(string palavra) {
+	string palavra;
+	set<string> documento;
+	map<string, int> tf;
+	set<string>::iterator itr1;
+	map<string, int>::iterator itr2;
+	if (indice_.pertence(palavra)) {
+		documento = m[palavra];
+	}
+
+	for (itr1 = documento.begin(); itr1 != documento.end(); itr1++) {
+		tf[*itr1] = indice_.ocorrencias(palavra, *itr1);
+	}
+
+	return tf[palavra];
+}
+	
