e2631b2938c0e1ce8bb2719a23dc4040fd50fbb0
Lucas Prado <42065138+Salobre@users.noreply.github.com>
2019-06-12 23:03:20 -0300

    Update and rename Tp.cpp to Indice_Invertido.cpp

--- Indice_Invertido.cpp
+++ Indice_Invertido.cpp
@@ -19,7 +19,7 @@ void File_reader::Ler() {
 		while (!doc.eof()) {
 			doc >> palavra;
 			transform(palavra.begin(), palavra.end(), palavra.begin(), ::tolower);
-			
+			palavra.erase(remove_if(palavra.begin(), palavra.end(), [](char c) {return !isalpha(c) && !isdigit(c);}), palavra.end());
 			set<string> valor = { "d" + to_string(i) + ".txt" };
 			pair<map<string, set<string>>::iterator, bool> teste;
 			teste = indice.insert(pair<string, set<string>>(palavra, valor));
@@ -49,6 +49,14 @@ void File_reader::Imprimir() {
 
 	}
 };
+map<string, set<string>> File_reader::mapa(){
+	return indice;
+
+}
+
+bool File_reader::pertence(string palavra){
+	return(indice.find(palavra) != indice.end());
+}
 
 int File_reader::doc_number(string c) {
 	int count = 0;
@@ -57,6 +65,35 @@ int File_reader::doc_number(string c) {
 	count = aux[c].size();
 	return count;
 	}
+
+int ocorrencias(string palavra_nova, string documento) {
+	int conta=0;
+	ifstream doc;
+	for (int i = 1; 1; i++) {
+
+		doc.open(documento);
+		if (!doc.is_open() && i == 1) {
+			cout << "Falha na leitura" << endl;
+			break;
+		}
+		if (!doc.is_open()) {
+			break;
+		}
+		
+		string palavra;
+		while (!doc.eof()) {
+			doc >> palavra;
+			transform(palavra.begin(), palavra.end(), palavra.begin(), ::tolower);
+			palavra.erase(remove_if(palavra.begin(), palavra.end(), [](char c) {return !isalpha(c) && !isdigit(c); }), palavra.end());
+			if (palavra == palavra_nova) {
+				conta++;
+			}
+		}
+		doc.close();
+	}
+	return conta;
+
+}
 
 
 
