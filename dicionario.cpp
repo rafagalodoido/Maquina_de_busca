cceafb47a228bb9d8e95f79e076a7cf214af3327
Lucas Prado <42065138+Salobre@users.noreply.github.com>
2019-06-13 02:36:18 -0300

    Update dicionario.cpp

--- dicionario.cpp
+++ dicionario.cpp
@@ -1,44 +1,84 @@
-
 #include "dicionario.h"
 using std::string;
 
-int dicionario::idf(string palavra) {
+double dicionario::idf(string palavra) {
 	//precisa definir o docs
 	indice_.Ler();
-	string palavra;
-	int freq;
-	int idf;
+	double freq;
+	double idf_;
 	map<string, set<string>>::iterator itr;
-	map<string,int> aux;
+	map<string,double> aux;
 	for (itr = indice_.mapa().begin(); itr != indice_.mapa().end(); itr++) {
 		palavra = itr->first;
 		N = indice_.doc_quantity();
 		nt = indice_.doc_number(palavra);
 		freq = N / nt;
-		idf = log2(freq);
-		aux.insert(pair<string, int>(palavra, idf));
+		idf_ = log2(freq);
+		aux.insert(pair<string, double>(palavra, idf_));
 	}
 
 	if (nt == 0) {
-		idf = 0;
+		idf_ = 0;
 		cout << "palavra não encontrada." << endl;
 	}
-	return idf;
+	return aux[palavra];
 }
-int dicionario::tf(string palavra) {
-	string palavra;
+
+double dicionario::tf(string palavra) {
+	indice_.Ler();
 	set<string> documento;
-	map<string, int> tf;
+	map<string, double> tf_;
 	set<string>::iterator itr1;
-	map<string, int>::iterator itr2;
+	m = indice_.mapa();
 	if (indice_.pertence(palavra)) {
 		documento = m[palavra];
 	}
 
 	for (itr1 = documento.begin(); itr1 != documento.end(); itr1++) {
-		tf[*itr1] = indice_.ocorrencias(palavra, *itr1);
+		tf_[*itr1] = indice_.ocorrencias(palavra, *itr1);
 	}
 
-	return tf[palavra];
+	return tf_[palavra];
 }
 	
+double dicionario::w(string palavra) {
+	dicionario d;
+	double w_;
+	w_ = d.idf(palavra) * d.tf(palavra);
+	return w_;
+}
+
+double dicionario::cosine_ranking() {
+	dicionario d;
+	string aux;
+	vector<double> pesq_;
+	ifstream doc;
+	for (int i = 1; 1; i++) {
+		doc.open("d" + to_string(i) + ".txt");
+		if (!doc.is_open() && i == 1) {
+			cout << "Falha na leitura" << endl;
+			break;
+		}
+		if (!doc.is_open()) {
+			break;
+		}
+		string documento;
+		string palavra;
+		while (!doc.eof()) {
+			doc >> palavra;
+			transform(palavra.begin(), palavra.end(), palavra.begin(), ::tolower);
+			palavra.erase(remove_if(palavra.begin(), palavra.end(), [](char c) {return !isalpha(c) && !isdigit(c); }), palavra.end());			
+			pesq_.push_back(d.w(palavra));
+			
+			documento = "d" + to_string(i) + ".txt";
+			mapa_busca.insert(pair<string, vector<double>>(documento, pesq_));
+			
+		}
+		doc.close();
+	}
+};
+
+map<string, vector<double>> dicionario:: map_return() {
+	return mapa_busca;
+}
+
