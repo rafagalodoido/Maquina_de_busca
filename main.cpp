da3416018d203d87f75a3214324ce2371671fca8
Lucas Prado <42065138+Salobre@users.noreply.github.com>
2019-06-13 14:46:13 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -45,7 +45,8 @@ int main() {
 	map<string, vector<double>>::iterator it;
 	for (it = map_aux.begin(); it != map_aux.end(); it++) {
 		for (double i = 0; i < pesquisa.size(); i++) {
-			if (it->second[i] - pesquisa[i] = 0 || it->second[i] - pesquisa[i] < 0, 2 || it->second[i] - pesquisa[i] >0, 2) {
+			double result = (it->second[i] * pesquisa[i])/sqrt((it->second[i]*it->second[i])*(pesquisa[i]*pesquisa[i])); 
+			if (final.sim(busca) == result) {
 				cout << "O documento que voce procura e"<< it->first << endl;
 			}
 		}
