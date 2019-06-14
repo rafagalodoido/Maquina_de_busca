0fb58e5b878692995b1596b795cadc6d585370b7
oliveiraigor <49527476+oliveiraigor@users.noreply.github.com>
2019-06-13 23:48:54 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,48 +6,30 @@
 #include <set>
 #include <algorithm>
 #include "dicionario.h"
-#include "Indice_Invetido.h"
+#include "Indice_Invertido.h"
 
 using namespace std;
 
 int main() {
+	File_reader arquivos;
 	double aux;
 	dicionario final;
 	vector<double> pesquisa;
 	vector<string>stringHolder;
 	string busca;
 	map<string, vector<double>> map_aux;
+		cout<<"Digite a palavra para a busca:" << endl;
+		cin >> busca; 
+		arquivos.Ler();
+		arquivos.Imprimir();
+
 	final.cosine_ranking();
 	map_aux = final.map_return();
-	cout << "Digite a palavra para a busca:" << endl;
-	while (cin >> busca) {
-		stringHolder.push_back(busca);
-	}
-	sort(stringHolder.begin(), stringHolder.end());
-	int ssize = stringHolder.size();
-	if (ssize == 0) {
-		cout << "No words" << endl;
-		return 1;
-	}
-
-	int wordCount = 1;
-	busca = stringHolder[0];
-	for (int i = 1; i < ssize; i++) {
-		if (busca != stringHolder[i]) {
-			aux = final.idf(busca) * wordCount;
-			pesquisa.push_back(aux);
-			wordCount = 0;
-			busca = stringHolder[i];
-		}
-		wordCount++;
-	}
-
-
 	map<string, vector<double>>::iterator it;
 	for (it = map_aux.begin(); it != map_aux.end(); it++) {
 		for (double i = 0; i < pesquisa.size(); i++) {
-			double result = (it->second[i] * pesquisa[i])/sqrt((it->second[i]*it->second[i])*(pesquisa[i]*pesquisa[i])); 
-			if (final.sim(busca) == result) {
+			double result = (it->second[i] * pesquisa[i])/sqrt((it->second[i]it->second[i])(pesquisa[i]*pesquisa[i])); 
+			if (final.sim(busca) == result|| final.sim(busca) - result>0.5) {
 				cout << "O documento que voce procura e"<< it->first << endl;
 			}
 		}
